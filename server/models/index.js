9e95234a0ff9bd282ca8bc1a65de67968951f251
Douglas Lyford <Douggyfr3sh@gmail.com>
2017-06-15 00:23:24 -0400

    refactored get handler for messages on db.

--- server/models/index.js
+++ server/models/index.js
@@ -11,20 +11,22 @@ module.exports = {
         ORDER BY messages.id DESC';
       var queryArgs = [];
 
-      db.query(queryString, queryArgs, function(err, results) {
-        if (err) { throw err; }
+      db.query(queryString, (err, results) => {
         callback(results);
       });
     },
-    post: function (msg, callback) {
-      //TO DO: reference id_rooms and id_users from the correct tables in order to store them in the messages table.
-      var msgsSql = 'INSERT INTO messages (text, timestamp, id_rooms, id_users) VALUE (?, ?, (SELECT id from rooms WHERE name = ? limit 1), (SELECT id from users WHERE name = ? limit 1))';
-      var msgParams = [msg.message, msg.createdAt, msg.roomname, msg.username];
 
-      db.query(msgsSql, msgParams, function(err, results) {
+    // a function which can be used to insert a message into the database
+    post: function (data, callback) {
+      //TO DO: reference id_rooms and id_users from the correct tables in order to store them in the messages table.
+      var queryString = 'INSERT INTO messages (text, id_rooms, id_users) \
+        VALUES (?, (SELECT id from rooms WHERE name = ? limit 1), \
+        (SELECT id from users WHERE name = ? limit 1))';
+
+      db.query(queryString, data, (err, results) => {
         callback(err, results);
       });
-    } // a function which can be used to insert a message into the database
+    }
   },
 
   users: {
